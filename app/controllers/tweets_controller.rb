bbfa33844d15c2169c40c03a1f40eba8607e4903
Nicole Hall <nicolealexandrahall@gmail.com>
2015-12-16 13:40:29 -0700

    materialize gem is not working

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,6 @@ class TweetsController < ApplicationController
   def index
     @screen_name = current_user.screen_name
     @tweets = current_user.tweets
-    # @description = current_user.description
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid                = auth_info.uid
       new_user.name               = auth_info.extra.raw_info.name
+      new_user.image              = auth_info.info.image
+      new_user.location           = auth_info.extra.raw_info.location
+      new_user.description        = auth_info.extra.raw_info.description
       new_user.screen_name        = auth_info.extra.raw_info.screen_name
       new_user.oauth_token        = auth_info.credentials.token
       new_user.oauth_token_secret = auth_info.credentials.secret
@@ -22,9 +25,5 @@ class User < ActiveRecord::Base
   def tweets
     client.user_timeline(self.uid.to_i)
   end
-
-  # def description
-  #   client.user_description(self.uid.to_i)
-  # end
 
 end
