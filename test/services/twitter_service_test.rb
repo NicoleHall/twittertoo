7273aad97ada5153e08204001b8258ae528e985d
Nicole Hall <nicolealexandrahall@gmail.com>
2015-12-17 12:15:27 -0700

    back to passing

--- test/services/twitter_service_test.rb
+++ test/services/twitter_service_test.rb
@@ -5,7 +5,7 @@ class TwitterServiceTest < ActiveSupport::TestCase
   attr_reader :service
 
   def setup
-    @service = TwitterService.new(User.new(name: "Cole Mersich", screen_name: "ColeMersich", uid: "250190173", oauth_token: ENV["access_token"], oauth_token_secret: ENV["access_token_secret"], created_at: "2015-12-17 01:23:17", updated_at: "2015-12-17 01:23:17", image: nil))
+    @service = TwitterService.new(User.new(name: "Cole Mersich", screen_name: "ColeMersich", uid: "250190173", oauth_token: ENV["oauth_token"], oauth_token_secret: ENV["oauth_token_secret"], created_at: "2015-12-17 01:23:17", updated_at: "2015-12-17 01:23:17", image: nil))
   end
 
   test 'tweets method show list of tweets' do
@@ -22,7 +22,7 @@ class TwitterServiceTest < ActiveSupport::TestCase
       tweets = service.tweets
       first_of_timeline  = @service.timeline.first
 
-      assert_equal 'The creators of the ColecoVision are back with a new cartridge-based console https://t.co/pkUm2RpPW6 https://t.co/LwQb0QCIfo',  first_of_timeline.text
+      assert_equal "Travel Marketers Embrace Virtual Reality as the Next Big Thing  https://t.co/NYef4y7MXT https://t.co/xYTy09HPb6",  first_of_timeline.text
     end
   end
 end
